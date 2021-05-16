97faa24516cca3d28854cb1c6910d8a3f1b44091
Ezequiel Ramirez <ezequielram@gmail.com>
2021-05-16 19:09:58 -0300

    js en carrito

--- js/simulador.js
+++ js/simulador.js
@@ -6,9 +6,11 @@ const multiplicacion = (a,b) => a * b;
 const iva = a => a * 0.21;
 const desc = a => a -(a * 0.1);
 const rec = a => a + (a * 0.1);
+let sumaTotal = 0;
 /* datos */
 const DATOS = [{
     "id": 1,
+   
     "nombre": "Stady",
     "orientacion": "Masculino",
     "tipo": "Descanso",
@@ -43,6 +45,7 @@ const DATOS = [{
 class Lentes{
     constructor(datos){
         this.id = parseInt(datos.id);
+        
         this.nombre = datos.nombre;
         this.orientacion = datos.orientacion;
         this.tipo = datos.tipo;
@@ -124,28 +127,28 @@ for (const iterador of productos) {
 let padreSelecciones = document.getElementById("filaDeSeleccionados");
 //CREO FUNCION PARA AGREGAR AL CARRITO UN PRODUCTO
 function agregarCarroId(prod) {
-    let contenedor2 = document.createElement("th");
-    contenedor2.classList.add('id');
-    contenedor2.innerHTML = `${prod.id}`;
-    padreSelecciones.appendChild(contenedor2);
+    let celda1 = document.createElement("th");
+    celda1.classList.add('id');
+    celda1.innerHTML = `${prod.id}`;
+    padreSelecciones.appendChild(celda1);
 }
 function agregarCarroFoto(prod) {
-    let contenedor3 = document.createElement("th");
-    contenedor3.classList.add('prodFoto');
-    contenedor3.innerHTML = `${prod.foto}`;
-    padreSelecciones.appendChild(contenedor3);
+    let celda2 = document.createElement("th");
+    celda2.classList.add('prodFoto');
+    celda2.innerHTML = `${prod.img}`;
+    padreSelecciones.appendChild(celda2);
 }
 function agregarCarroNombre(prod) {
-    let contenedor4 = document.createElement("th");
-    contenedor4.classList.add('prodNombre');
-    contenedor4.innerHTML = `${prod.nombre}`;
-    padreSelecciones.appendChild(contenedor4);
+    let celda3 = document.createElement("th");
+    celda3.classList.add('prodNombre');
+    celda3.innerHTML = `${prod.nombre}`;
+    padreSelecciones.appendChild(celda3);
 }
 function agregarCarroPrecio(prod) {
-    let contenedor5 = document.createElement("th");
-    contenedor5.classList.add('prodPrecio');
-    contenedor5.innerHTML = `${prod.precio}`;
-    padreSelecciones.appendChild(contenedor5);
+    let celda4 = document.createElement("th");
+    celda4.classList.add('prodPrecio');
+    celda4.innerHTML = `${prod.precio}`;
+    padreSelecciones.appendChild(celda4);
 }  
     
     
@@ -179,10 +182,10 @@ while (cantidad < 1 || cantidad > 5){
 }
 
 function agregarCarroCantidad(prod) {
-    let contenedor6 = document.createElement("th");
-    contenedor6.classList.add('prodCantidad');
-    contenedor6.innerHTML = `${cantidad}`;
-    padreSelecciones.appendChild(contenedor6);
+    let celda5 = document.createElement("th");
+    celda5.classList.add('prodCantidad');
+    celda5.innerHTML = `${cantidad}`;
+    padreSelecciones.appendChild(celda5);
 } 
 agregarCarroCantidad(cantidad); 
  //OPERACIONES
@@ -198,21 +201,32 @@ console.log("Costo con tarjeta:");
 console.log(recargo);
 
 //FUNCION PARA MOSTRAR SU IMPORTE A ABONAR
-//MUESTRO LA SELECCIÓN POR HTML
-function funcionDeImporteEfectivo(dato){
-let padreSelecciones = document.getElementById("filaDeSeleccionados");
-let contenedor2 = document.createElement("div");
-contenedor2.classList.add("col-6");
-contenedor2.classList.add("container_product");
-contenedor2.classList.add("p3");
-contenedor2.classList.add("container_product__select");
-contenedor2.innerHTML = ` <h2>Importe a abonar en Efectivo:</h2>
-                        <h3>$ ${dato}</h3></br>
-                        <p> "Gracias por tu compra".!!!</p>
-`;
-padreSelecciones.appendChild(contenedor2);
+//MUESTRO LA SELECCIÓN POR HTML EN UL
+function funcionDeImporte(dato){
+let padreUl = document.getElementById("listaImporte");
+let lista = document.createElement("li");
+lista.classList.add("lista_importe__detalle");
+lista.innerHTML = ` Producto -> ${dato.nombre}
+<span>$ ${dato.precio * cantidad}</span>
+                    `;
+padreUl.appendChild(lista);
+sumaTotal += (dato.precio*cantidad);
 }
-function funcionDeImporteUnpago(dato){
+funcionDeImporte(encontrado);
+
+//FUNCION PARA SUMA TOTAL DE IMPORTE
+//MUESTRO LA SUMA TOTAL EN UL
+function funcionImporteTotal(dato){
+    let padreUl = document.getElementById("listaImporte");
+    let lista = document.createElement("li");
+    lista.classList.add("lista_importe__total");
+    lista.innerHTML = ` Total=
+    <span>$ ${dato}</span>
+                        `;
+    padreUl.appendChild(lista);
+    }
+    funcionImporteTotal(sumaTotal);
+/* function funcionDeImporteUnpago(dato){
 let padreSelecciones = document.getElementById("filaDeSeleccionados");
 let contenedor2 = document.createElement("div");
 contenedor2.classList.add("col-6");
@@ -224,8 +238,8 @@ contenedor2.innerHTML = ` <h2>Importe a abonar en Un Pago:</h2>
                         <p> "Gracias por tu compra".!!!</p>
 `;
 padreSelecciones.appendChild(contenedor2);
-}
-function funcionDeImporteRecargo(dato){
+} */
+/* function funcionDeImporteRecargo(dato){
 let padreSelecciones = document.getElementById("filaDeSeleccionados");
 let contenedor2 = document.createElement("div");
 contenedor2.classList.add("col-6");
@@ -237,9 +251,9 @@ contenedor2.innerHTML = ` <h2>Importe a abonar en Cuotas:</h2>
                         <p> "Gracias por tu compra".!!!</p>
 `;
 padreSelecciones.appendChild(contenedor2);
-}
+} */
 //MEDIO DE PAGO
-let metodoPago = parseInt(prompt("Ingrese la forma de pago (DIGITE SU NUMERO): \n 1 - Efectivo \n 2 - Tarjeta un pago \n 3 - Tarjeta en cuotas"));
+//let metodoPago = parseInt(prompt("Ingrese la forma de pago (DIGITE SU NUMERO): \n 1 - Efectivo \n 2 - Tarjeta un pago \n 3 - Tarjeta en cuotas"));
 
 if (metodoPago ==1 || metodoPago ==2 || metodoPago ==3) {
     switch (metodoPago) {
@@ -260,7 +274,7 @@ if (metodoPago ==1 || metodoPago ==2 || metodoPago ==3) {
             break;
     }
 } else {
-    metodoPago = parseInt(prompt("Ingrese la forma de pago (DIGITE SU NUMERO): \n 1 - Efectivo \n 2 - Tarjeta un pago \n 3 - Tarjeta en cuotas"));
+   // metodoPago = parseInt(prompt("Ingrese la forma de pago (DIGITE SU NUMERO): \n 1 - Efectivo \n 2 - Tarjeta un pago \n 3 - Tarjeta en cuotas"));
 }
 //MODIFICAR PRODUCTO A VENDIDO= TRUE
  for (const iterador of productos) {
